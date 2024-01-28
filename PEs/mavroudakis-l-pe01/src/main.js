e3df43cbb5a15deaa8829bb13dc6b4d204191145
ltm7725 <ltm7725@g.rit.edu>
2024-01-28 14:47:41 -0500

    Updated files

---
+++ PEs/mavroudakis-l-pe01/src/main.js
@@ -0,0 +1,29 @@
+"use strict";
+
+function updateBabble() {
+    // Assigning same calculated index to all 3 of these would work to avoid repetitive code, but we want variation
+    let word1 = words1[Math.floor(Math.random() * words1.length)];
+    let word2 = words2[Math.floor(Math.random() * words2.length)];
+    let word3 = words3[Math.floor(Math.random() * words3.length)];
+
+    let theBabble = word1 + " " + word2 + " " + word3;
+    document.querySelector("#output").innerHTML = theBabble;
+}
+
+const words1 = ["Acute", "Aft", "Anti-matter", "Bipolar", "Cargo", "Command", "Communication", "Computer", "Deuterium", "Dorsal", "Emergency", "Engineering", "Environmental", "Flight", "Fore", "Guidance", "Heat", "Impulse", "Increased", "Inertial", "Infinite", "Ionizing", "Isolinear", "Lateral", "Linear", "Matter", "Medical", "Navigational", "Optical", "Optimal", "Optional", "Personal", "Personnel", "Phased", "Reduced", "Science", "Ship's", "Shuttlecraft", "Structural", "Subspace", "Transporter", "Ventral"];
+
+const words2 = ["Propulsion", "Dissipation", "Sensor", "Improbability", "Buffer", "Graviton", "Replicator", "Matter", "Anti-matter", "Organic", "Power", "Silicon", "Holographic", "Transient", "Integrity", "Plasma", "Fusion", "Control", "Access", "Auto", "Destruct", "Isolinear", "Transwarp", "Energy", "Medical", "Environmental", "Coil", "Impulse", "Warp", "Phaser", "Operating", "Photon", "Deflector", "Integrity", "Control", "Bridge", "Dampening", "Display", "Beam", "Quantum", "Baseline", "Input"];
+
+const words3 = ["Chamber", "Interface", "Coil", "Polymer", "Biosphere", "Platform", "Thruster", "Deflector", "Replicator", "Tricorder", "Operation", "Array", "Matrix", "Grid", "Sensor", "Mode", "Panel", "Storage", "Conduit", "Pod", "Hatch", "Regulator", "Display", "Inverter", "Spectrum", "Generator", "Cloud", "Field", "Terminal", "Module", "Procedure", "System", "Diagnostic", "Device", "Beam", "Probe", "Bank", "Tie-In", "Facility", "Bay", "Indicator", "Cell"];
+
+console.log(words1[0]);
+
+function init() {
+    const button = document.querySelector("#myButton");
+    // Handler
+    //button.onclick = updateBabble;
+    button.addEventListener("click", () => { updateBabble() });
+    updateBabble();
+}
+
+window.onload = init;
