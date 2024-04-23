382c8aed2a9bc6fb9c826bdf4125bed8372b6e76
Luke Mavroudakis (RIT Student) <ltm7725@rit.edu>
2024-04-23 10:48:18 -0400

    Started starter

---
+++ Homeworks/mavroudakis-l-hw4-starter/src/main.js
@@ -0,0 +1,41 @@
+import * as map from "./map.js";
+import * as ajax from "./ajax.js";
+
+// I. Variables & constants
+// NB - it's easy to get [longitude,latitude] coordinates with this tool: http://geojson.io/
+const lnglatNYS = [-75.71615970715911, 43.025810763917775];
+const lnglatUSA = [-98.5696, 39.8282];
+
+
+// II. Functions
+const setupUI = () => {
+
+	// NYS Zoom 5.2
+	document.querySelector("#btn1").onclick = () => {
+		map.setZoomLevel(5.2);
+		map.setPitchAndBearing(0, 0);
+		map.flyTo(lnglatNYS);
+	}
+
+	// NYS isometric view
+	document.querySelector("#btn2").onclick = () => {
+		map.setZoomLevel(5.5);
+		map.setPitchAndBearing(45, 0);
+		map.flyTo(lnglatNYS);
+	}
+
+	// World zoom 0
+	document.querySelector("#btn3").onclick = () => {
+		map.setZoomLevel(3);
+		map.setPitchAndBearing(0, 0);
+		map.flyTo(lnglatUSA);
+	}
+
+}
+
+const init = () => {
+	map.initMap(lnglatNYS);
+	setupUI();
+};
+
+init();
